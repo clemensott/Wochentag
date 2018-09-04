587f58b11ac1f5280669a56398793725ee401f39
ShelbyBoss <42971547+ShelbyBoss@users.noreply.github.com>
2018-09-04 20:07:24 +0200

    Initial commit

---
+++ Wochentag/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Wochentag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_tag = new System.Windows.Forms.Label();
+            this.lbl_monat = new System.Windows.Forms.Label();
+            this.lbl_jahr = new System.Windows.Forms.Label();
+            this.tbx_tag = new System.Windows.Forms.TextBox();
+            this.tbx_monat = new System.Windows.Forms.TextBox();
+            this.tbx_jahr = new System.Windows.Forms.TextBox();
+            this.btn_berechnen = new System.Windows.Forms.Button();
+            this.tbx_ausgabe = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_tag
+            // 
+            this.lbl_tag.AutoSize = true;
+            this.lbl_tag.Location = new System.Drawing.Point(38, 49);
+            this.lbl_tag.Name = "lbl_tag";
+            this.lbl_tag.Size = new System.Drawing.Size(26, 13);
+            this.lbl_tag.TabIndex = 0;
+            this.lbl_tag.Text = "Tag";
+            // 
+            // lbl_monat
+            // 
+            this.lbl_monat.AutoSize = true;
+            this.lbl_monat.Location = new System.Drawing.Point(74, 50);
+            this.lbl_monat.Name = "lbl_monat";
+            this.lbl_monat.Size = new System.Drawing.Size(37, 13);
+            this.lbl_monat.TabIndex = 1;
+            this.lbl_monat.Text = "Monat";
+            // 
+            // lbl_jahr
+            // 
+            this.lbl_jahr.AutoSize = true;
+            this.lbl_jahr.Location = new System.Drawing.Point(124, 49);
+            this.lbl_jahr.Name = "lbl_jahr";
+            this.lbl_jahr.Size = new System.Drawing.Size(27, 13);
+            this.lbl_jahr.TabIndex = 2;
+            this.lbl_jahr.Text = "Jahr";
+            // 
+            // tbx_tag
+            // 
+            this.tbx_tag.Location = new System.Drawing.Point(36, 66);
+            this.tbx_tag.Name = "tbx_tag";
+            this.tbx_tag.Size = new System.Drawing.Size(32, 20);
+            this.tbx_tag.TabIndex = 3;
+            // 
+            // tbx_monat
+            // 
+            this.tbx_monat.Location = new System.Drawing.Point(75, 66);
+            this.tbx_monat.Name = "tbx_monat";
+            this.tbx_monat.Size = new System.Drawing.Size(34, 20);
+            this.tbx_monat.TabIndex = 4;
+            // 
+            // tbx_jahr
+            // 
+            this.tbx_jahr.Location = new System.Drawing.Point(115, 66);
+            this.tbx_jahr.Name = "tbx_jahr";
+            this.tbx_jahr.Size = new System.Drawing.Size(47, 20);
+            this.tbx_jahr.TabIndex = 5;
+            // 
+            // btn_berechnen
+            // 
+            this.btn_berechnen.Location = new System.Drawing.Point(61, 103);
+            this.btn_berechnen.Name = "btn_berechnen";
+            this.btn_berechnen.Size = new System.Drawing.Size(75, 23);
+            this.btn_berechnen.TabIndex = 6;
+            this.btn_berechnen.Text = "Berechnen";
+            this.btn_berechnen.UseVisualStyleBackColor = true;
+            this.btn_berechnen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbx_ausgabe
+            // 
+            this.tbx_ausgabe.Enabled = false;
+            this.tbx_ausgabe.Location = new System.Drawing.Point(61, 143);
+            this.tbx_ausgabe.Name = "tbx_ausgabe";
+            this.tbx_ausgabe.Size = new System.Drawing.Size(75, 20);
+            this.tbx_ausgabe.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(203, 192);
+            this.Controls.Add(this.tbx_ausgabe);
+            this.Controls.Add(this.btn_berechnen);
+            this.Controls.Add(this.tbx_jahr);
+            this.Controls.Add(this.tbx_monat);
+            this.Controls.Add(this.tbx_tag);
+            this.Controls.Add(this.lbl_jahr);
+            this.Controls.Add(this.lbl_monat);
+            this.Controls.Add(this.lbl_tag);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Wochentag";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_tag;
+        private System.Windows.Forms.Label lbl_monat;
+        private System.Windows.Forms.Label lbl_jahr;
+        private System.Windows.Forms.TextBox tbx_tag;
+        private System.Windows.Forms.TextBox tbx_monat;
+        private System.Windows.Forms.TextBox tbx_jahr;
+        private System.Windows.Forms.Button btn_berechnen;
+        private System.Windows.Forms.TextBox tbx_ausgabe;
+    }
+}
+
---
+++ Wochentag/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wochentag
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int tag = 0, tag_e, monat = 0, monat_e = 0, jahr = 0, jahr_hin = 0, jahr_hin_e, jahr_vor = 0, jahr_vor_e, schalt = 0, i, j;
+            int[] monate = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            string ergebnis = "Fehler";
+            bool test = true, fehler = false;
+
+            try
+            {
+                tag = Convert.ToInt32(tbx_tag.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Diesen Tag gibt es nicht", "Fehler");
+
+                fehler = true;
+            }
+
+            if (!fehler)
+            {
+                try
+                {
+                    monat = Convert.ToInt32(tbx_monat.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Diesen Monat gibt es nicht", "Fehler");
+
+                    fehler = true;
+                }
+
+            }
+
+            if (!fehler)
+            {
+                try
+                {
+                    jahr = Convert.ToInt32(tbx_jahr.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Dieses Jahr gibt es nicht", "Fehler");
+
+                    fehler = true;
+                }
+            }
+
+            if (!fehler)
+            {
+
+            if (jahr == 0)
+            {
+                string message = "Bist du Christ?";
+                string caption = "Glaubensfrage";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result;
+
+                result = MessageBox.Show(message, caption, buttons);
+
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    if (jahr == 0)
+                    {
+                        tbx_ausgabe.Text = "Problem";
+                        tbx_ausgabe.TextAlign = HorizontalAlignment.Center;
+
+                        MessageBox.Show("Es gibt im Christentum kein Jahr  0", "Problem");
+
+                        fehler = true;
+                    }
+                    else
+                    {
+                    jahr = jahr - 1;
+                    }
+                }
+            }
+            }
+
+            if (!fehler)
+            {
+            for (i = 0; test; i++)
+            {
+                j = jahr - (i * 100);
+
+                if (j < 100)
+                {
+                    jahr_vor = i;
+                    jahr_hin = j;
+
+                    test = false;
+                }
+            }
+     
+                if (monat > 12 || monat < 1)
+                {
+                    fehler = true;
+
+                    MessageBox.Show("Diesen Monat gibt es nicht", "Fehler");
+                }
+                else
+                {
+                    if (tag > (monate[(monat - 1)]) || tag < 1)
+                    {
+                        if ((jahr % 4) == 0 && monat == 2 && tag == 29)
+                        {
+
+                        }
+                        else
+                        {
+                            fehler = true;
+
+                            MessageBox.Show("Diesen Tag gibt es nicht", "Fehler");
+                        }
+                    }
+                }
+            }
+
+            if (!fehler)
+            {
+                tag_e = tag % 7;
+
+                for (i = 1; i < monat; i++)
+                {
+                    if (i == 1)
+                    {
+                        monat_e = 3;
+                    }
+                    else
+                    {
+                        monat_e = monat_e + (monate[(i - 1)] % 7);
+                    }
+
+                    if (monat_e > 6)
+                    {
+                        monat_e = monat_e % 7;
+                    }
+                }
+
+                jahr_vor_e = (3 - (jahr_vor % 4)) * 2;
+
+                jahr_hin_e = (jahr_hin + (jahr_hin / 4)) % 7;
+
+                if ((jahr % 4) == 0 && monat <= 2)
+                {
+                    schalt = 6;
+                }
+
+                i = tag_e + monat_e + jahr_vor_e + jahr_hin_e + schalt;
+
+                if (i > 6)
+                {
+                    i = i % 7;
+                }
+
+                if (i == 0)
+                {
+                    ergebnis = "Sonntag";
+                }
+
+                if (i == 1)
+                {
+                    ergebnis = "Montag";
+                }
+
+                if (i == 2)
+                {
+                    ergebnis = "Dienstag";
+                }
+
+                if (i == 3)
+                {
+                    ergebnis = "Mittwoch";
+                }
+
+                if (i == 4)
+                {
+                    ergebnis = "Donnerstag";
+                }
+
+                if (i == 5)
+                {
+                    ergebnis = "Freitag";
+                }
+
+                if (i == 6)
+                {
+                    ergebnis = "Samstag";
+                }
+
+
+                tbx_ausgabe.Text = ergebnis;
+            }
+
+            tbx_ausgabe.TextAlign = HorizontalAlignment.Center;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
